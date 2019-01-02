d6604d529540902640d28321c9e96090bbe3801b
AaditT <aadit.trivedi03@gmail.com>
2019-01-01 22:23:43 -0800

    update

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
+def updateDB():
+    types = []
+    counts = []
+    dates = []
+    tFile = open("type.txt", 'r')
+    for line in tFile.readlines():
+        types.append(str(line.rstrip()))
+    cFile = open("count.txt", 'r')
+    for line in cFile.readlines():
+        counts.append(str(line.rstrip()))
+    dFile = open("date.txt", 'r')
+    for line in dFile.readlines():
+        dates.append(str(line.rstrip()))
+    return [counts, dates, types]
+
 def getDate():
     x = datetime.datetime.now()
     return str(x.strftime("%x"))
@@ -12,13 +27,22 @@ def home():
 
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    updateDB()
+    return render_template('index.html',counts=updateDB()[0],dates=updateDB()[1],types=updateDB()[2])
 
 @app.route('/log', methods=['GET', 'POST'])
 def log():
     if request.method == 'POST':
-        myFile = open("log.txt", 'r+')
-        myFile.write(request.form['count'])
+        workout_type = str(request.form['workout'])
+        workout_date = str(request.form['start'])
+        workout_count = str(request.form['count'])
+        tFile = open("type.txt", 'a')
+        tFile.write(workout_type+"\n")
+        dFile = open("date.txt", 'a')
+        dFile.write(workout_date+"\n")
+        cFile = open("count.txt", 'a')
+        cFile.write(workout_count+"\n")
+        return redirect(url_for('index'))
     return render_template('log.html')
 
 
